9e0b01d82805ab44662bf0caaeece7e8799c8794
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-03-18 21:03:25 -0700

    define method for retrieving a specific book

--- controller/book_controller.js
+++ controller/book_controller.js
@@ -19,25 +19,6 @@ module.export = {
             res.status(422).json(err);
         })
     
-    },
-
-    save: function(req, res) {
-        db.Book.create(req.body)
-        .then(dbModel => res.json(dbModel))
-        .catch(err => {
-            console.log(err);
-            res.status(422).json(err)
-        })
-    },
-
-    remove: function (req, res) {
-        db.Book.findById({ id: req.params.id })
-        .then(db => dbModel.remove())
-        .then(dbModel => res.json(dbModel))
-        .catch(err => {
-            console.log(err);
-            res.status(422).json(err)
-        })
     }
     
 
