af9df4c830f8f95d18c0a4ecb6d15c0d5168b2f9
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-03-20 11:03:39 -0700

    defined state properties

---
+++ client/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React, {useState} from "react";
+import SearchBar from "../components/SearchBar";
+import Wrapper from "../components/Wrapper";
+
+const Home = () => {
+    const [dataSearch, setDataSearch] = useState({
+        books: [],
+        searchedBook: "",
+        savedBooks: [],
+
+    });
+
+
+
+
+}
+
+export default Home;
