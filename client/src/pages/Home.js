6cd55fcf4a49aeeb5bab065e69a6247808602c99
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-03-20 11:25:32 -0700

    called API to get a list of searched book and passed it to searchbar component to display

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,8 @@
 import React, {useState} from "react";
 import SearchBar from "../components/SearchBar";
 import Wrapper from "../components/Wrapper";
-
+import BookCard from "../components/BookCard";
+import API from "../utils/API";
 const Home = () => {
     const [dataSearch, setDataSearch] = useState({
         books: [],
@@ -19,12 +20,25 @@ const handleSearchChange = event => {
        })
        console.log(value)
 }
-
+const handleSubmit = event => {
+    API.getBook(dataSearch.searchedBook)
+    .then(response => {
+        setDataSearch({
+            ...dataSearch,
+            books: response.data.items
+        })
+    })
+}
 
 return (
     <Wrapper>
-        <SearchBar handleInputChange={handleSearchChange} />
-      
+        <SearchBar handleSubmit={handleSubmit} handleInputChange={handleSearchChange} search={dataSearch.searchedBook} />
+      {console.log(dataSearch.books)}
+      {dataSearch.books.length > 0 ? dataSearch.books.map(book => {
+          return (
+            <BookCard book={book} />
+          )
+      }) : ""}
       
     </Wrapper>
 )
